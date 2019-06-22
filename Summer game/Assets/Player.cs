f14a8cb86371791144158a309472d9094840ab23
Garrett <gmoseke@gmail.com>
2019-06-22 10:26:50 -0600

    Player Glide Implementation

--- Summer game/Assets/Player.cs
+++ Summer game/Assets/Player.cs
@@ -8,23 +8,34 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float dashSpeed;
     [SerializeField] private float gravAmt;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float startDashTime;
+    [SerializeField] private float glideDelayTimer;
+    [SerializeField] private float glideMoveModifier;
 
-    [SerializeField]private float dashTime;
-    [SerializeField]private float startDashTime;
+    [SerializeField] private int glideGravModifier;
 
-    private GameObject movingPlatform;
-    private Vector3 mpOffset;
+
+    //deprecated
+    /*    private GameObject movingPlatform;
+        private Vector3 mpOffset;*/
+
 
     //-1 is left, 1 is right, 0 is not moving
     private int direction;
 
-    [SerializeField]private bool isGrounded, isDashing;
+    [SerializeField]private bool isGrounded, isDashing, isGliding;
+    private bool canGlide = false;
+    private bool startGlideTimer = false;
 
     private Vector3 moveVector;
 
     private Rigidbody2D playerRB;
 
     private float currJumpForce;
+    private float glideGravAmt;
+    private float glideDelayTimerCount;
+  
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +43,12 @@ public class Player : MonoBehaviour
         moveVector = new Vector3(moveAmount, 0f);
         playerRB = this.GetComponent<Rigidbody2D>();
         playerRB.gravityScale = gravAmt;
+        glideGravAmt = gravAmt / glideGravModifier;
+        glideDelayTimerCount = glideDelayTimer;
 
         isGrounded = true;
         isDashing = false;
+        isGliding = false;
 
         dashTime = startDashTime;
 
@@ -42,7 +56,6 @@ public class Player : MonoBehaviour
 
         currJumpForce = jumpForce;
 
-        movingPlatform = null;
     }
 
     // Update is called once per frame
@@ -51,20 +64,51 @@ public class Player : MonoBehaviour
 
         if(Input.GetKey(KeyCode.D))
         {
-            this.transform.position += moveVector;
+            //If we're gliding, modify our movement to be a little faster
+            if (isGliding)
+                this.transform.position += moveVector * glideMoveModifier;
+            else
+                this.transform.position += moveVector;
             direction = 1;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            this.transform.position -= moveVector;
+            //If we're gliding, modify our movement to be a little faster
+            if (isGliding)
+                this.transform.position -= moveVector * glideMoveModifier;
+            else
+                this.transform.position -= moveVector;
             direction = -1;
         }
 
+        //Jump
         if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             playerRB.AddForce(Vector2.up * currJumpForce, ForceMode2D.Impulse);
+            startGlideTimer = true;
         }
 
+        //If I let go of spacebar, we should stop gliding for this frame
+        if (Input.GetKeyUp(KeyCode.Space) && isGliding)
+        {
+            isGliding = false;
+        }
+
+        //Glide, only if not dashing or on the ground
+        if (Input.GetKey(KeyCode.Space) && !isGrounded && !isDashing && canGlide)
+        {
+            //if we just started gliding, zero out our velocity so we stop jumping as soon as we start the glide
+            if(!isGliding)
+            {
+                Debug.Log("stopped jump");
+                playerRB.velocity = Vector2.zero;
+            }
+            //Lower gravity, mark us as gliding
+            playerRB.gravityScale = glideGravAmt;
+            isGliding = true;
+        }
+
+        //Dash
         if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
         {
             isDashing = true;
@@ -85,12 +129,13 @@ public class Player : MonoBehaviour
             
         }
 
-        //if we're not dashing, turn gravity back on pls
-        if (isDashing == false)
+        //if we're not dashing/gliding, turn gravity back on pls
+        if (!isDashing && !isGliding)
         {
             playerRB.gravityScale = gravAmt;
         }
-        else
+        //If we are, decrease our timer
+        if(isDashing)
         {
             //decrease dash time
             dashTime -= Time.deltaTime;
@@ -101,6 +146,17 @@ public class Player : MonoBehaviour
                 isDashing = false;
             }
         }
+        //If we are in the middle of a jump, start our glide timer so we delay when we can glide
+        if(startGlideTimer)
+        {
+            glideDelayTimerCount -= Time.deltaTime;
+            if(glideDelayTimerCount <= 0)
+            {
+                glideDelayTimerCount = glideDelayTimer;
+                canGlide = true;
+                startGlideTimer = false;
+            }
+        }
        
     }
 
@@ -108,6 +164,8 @@ public class Player : MonoBehaviour
     {
         if (col.gameObject.tag.Contains("Platform"))
             isGrounded = true;
+            isGliding = false;
+            canGlide = false;
     }
 
     void OnCollisionEnter2D(Collision2D col)
