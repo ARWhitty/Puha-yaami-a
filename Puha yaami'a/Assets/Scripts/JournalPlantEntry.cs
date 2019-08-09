422f0fe23bb0bce52a97f5cf09bf208194888f25
Garrett <gmoseke@gmail.com>
2019-08-08 19:56:40 -0600

    Jump Changes, Scriptable journal entries
    Player now only gets 1 jump when walking off a ledge/falling off a cruble platform, animations updated accordingly.
    
    Added the ability to make journal entry objects to the editor for easier workflow

---
+++ Puha yaami'a/Assets/Scripts/JournalPlantEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Journal Entry", menuName = "Journal Entry/Plant")]
+public class JournalEntry : ScriptableObject
+{
+    public string title;
+    public string description;
+
+    public Sprite image;
+}
---
+++ Puha yaami'a/Assets/Scripts/JournalPoemEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Journal Entry", menuName = "Journal Entry/Poem")]
+
+public class JournalPoemEntry : ScriptableObject
+{
+    public string title;
+
+    [TextArea(1, 25)]
+    public string text;
+
+    public Sprite image;
+}
