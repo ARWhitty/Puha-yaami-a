9f5a8ffe37879886354140dc6cbbeaefdc7c5e77
Garrett <gmoseke@gmail.com>
2019-09-02 16:07:54 -0600

    Lots of backend
    Gave player a small "gimme" zone when jumping, Tons of backend code for saving/loading various chunks of data

---
+++ Puha yaami'a/Assets/Scripts/Data Mangement/JournalData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class JournalData
+{
+    public bool[] unlockedData;
+
+    public JournalData(JournalManager journalMgr)
+    {
+        //3 entries per page
+        unlockedData = new bool[journalMgr.pages.Count * 3];
+
+        //pull stuff out to a temp list
+        List<JournalEntry> allEntries = new List<JournalEntry>();
+        foreach(JournalPage page in journalMgr.pages)
+        {
+            allEntries.Add(page.top);
+            allEntries.Add(page.mid);
+            allEntries.Add(page.bottom);
+        }
+
+        //populate front to back
+        for(int i = 0; i < unlockedData.Length; i++)
+        {
+            unlockedData[i] = allEntries[i].unlocked;
+        }
+    }
+}
