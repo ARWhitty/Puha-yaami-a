2300a805862777091b617531f083f0008ad9c0f2
Dylan Pinkney <dpinkney99@gmail.com>
2020-02-04 11:47:16 -0700

    Redrock 2 Revamp 2 of 3 & Added One-Way Platforms

---
+++ Puha yaami'a/Assets/Scripts/Object Behaviors/OneWayPlat.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayPlat : MonoBehaviour
+{
+    public BoxCollider2D platform;
+    public bool oneWay = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        platform.enabled = !oneWay;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        oneWay = true;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        oneWay = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        oneWay = false;
+    }
+}
